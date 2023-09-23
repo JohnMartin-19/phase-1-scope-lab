2c0af52c6e12c34389e11b6639276501d332f331
John Mburu <johnnkonge2020@gmail.com>
2023-09-23 06:10:33 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'John';
+
+function upper(){
+    caps = customerName.toLocaleUpperCase();
+    console.log(caps)
+}
+upper()
+
+
+bestCustomer = 'bob';
+function setBestCustomer(){
+    let bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+setBestCustomer();
+
+function overrideBest(){
+    const bestCustomer = 'John';
+    console.log(bestCustomer);
+}
+overrideBest();
+
+const leastFav = 'Peter';
+function change(){
+    leastFav = 'Mburuu';
+    console.log(leastFav);
+}
+change();
